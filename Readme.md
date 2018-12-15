df450ceb62de3b252a0d5d9bf99807821c84c3f5
jochen@homeland <no-go@localhost>
2018-12-15 03:39:33 +0100

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,54 @@
+# Ca Put Hown (Kaputt hauen)
+
+Makes small preview images and generate a mp4 file in `Documents/de.digisocken.caputhown/` folder.
+Select **create** from the options menu!!
+
+![Layout](app/src/main/res/mipmap-xxxhdpi/ic_launcher.png)
+
+## Get the App
+
+- for Android 4.4+ [APK file](https://gitlab.com/deadlockz/caputhown/raw/master/app/release/de.digisocken.caputhown.apk)
+
+## License
+
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or distribute this 
+software, either in source code form or as a compiled binary, for any purpose, 
+commercial or non-commercial, and by any means.
+
+In jurisdictions that recognize copyright laws, the author or authors of this software 
+dedicate any and all copyright interest in the software to the public domain. We make 
+this dedication for the benefit of the public at large and to the detriment of our 
+heirs and successors. We intend this dedication to be an overt act of relinquishment 
+in perpetuity of all present and future rights to this software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, 
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to [http://unlicense.org](http://unlicense.org)
+
+## Privacy Policy
+
+### Personal information.
+
+Personal information is data that can be used to uniquely identify or contact a
+single person. I DO NOT collect, transmit, store or use any personal information while you use this app.
+
+### Non-Personal information.
+
+I DO NOT collect non-personal information like user's behavior:
+
+ -  to solve App problems
+ -  to show personalized ads
+
+The Google Play Store collects non-personal information such as the data of install (country and equipment).
+I did NOT add any Google or ad keys or codes for marketing feedback, Ads or payment systems!
+
+### Privacy Questions.
+
+If you have any questions or concerns about my Privacy Policy or data processing, please contact me.
---
+++ app/src/main/java/de/digisocken/caputhown/ScrollingActivity.java
@@ -0,0 +1,172 @@
+package de.digisocken.caputhown;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.style.ImageSpan;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import org.jcodec.api.android.AndroidSequenceEncoder;
+import org.jcodec.common.io.FileChannelWrapper;
+import org.jcodec.common.io.NIOUtils;
+import org.jcodec.common.model.Rational;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class ScrollingActivity extends AppCompatActivity {
+    private static final int CAMERA_REQUEST = 1888;
+    private ArrayList<Bitmap> bmps;
+    private FloatingActionButton fab;
+
+    String mCurrentPhotoPath;
+
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        bmps = new ArrayList<>();
+        setContentView(R.layout.activity_scrolling);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        textView = (TextView) findViewById(R.id.largeText);
+
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String timeStamp = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
+                File storageDir = getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS);
+                File image;
+                try {
+                    image = File.createTempFile(
+                            timeStamp,
+                            ".jpg",
+                            storageDir
+                    );
+                    mCurrentPhotoPath = image.getAbsolutePath();
+
+                    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                        if (image != null) {
+                            Uri photoURI = FileProvider.getUriForFile(getApplicationContext(),
+                                    "de.digisocken.caputhown.fileprovider",
+                                    image
+                            );
+                            // todo: use this high res picture
+                            //takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                            startActivityForResult(takePictureIntent, CAMERA_REQUEST);
+                        }
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_scrolling, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_movie) {
+            FileChannelWrapper out = null;
+
+            File file = null;
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
+                file = new File(
+                        Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS),
+                        getPackageName()
+                );
+            } else {
+                file = new File(Environment.getExternalStorageDirectory() + "/Documents/"+getPackageName());
+            }
+
+            String path = file.getPath() +"/"+ new SimpleDateFormat("yyyyMMddHHmmss").format(new Date())+".mp4";
+            try {
+                if (!file.exists()) file.mkdirs();
+                file = new File(path);
+                if (!file.exists()) file.createNewFile();
+            } catch (Exception e) {
+            }
+
+            try {
+                out = NIOUtils.writableFileChannel(file.getAbsolutePath());
+                AndroidSequenceEncoder encoder = new AndroidSequenceEncoder(out, Rational.R(15, 1));
+                for (Bitmap bitmap : bmps) {
+                    encoder.encodeImage(bitmap);
+                }
+                encoder.finish();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                NIOUtils.closeQuietly(out);
+            }
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            Bitmap mutableBitmap = Bitmap.createScaledBitmap(
+                    photo,
+                    2*photo.getWidth(),
+                    2*photo.getHeight(),
+                    true
+            );
+            bmps.add(mutableBitmap);
+
+            CharSequence sp = Integer.toString(bmps.size())+"  \n";
+            SpannableStringBuilder ssb = new SpannableStringBuilder(sp);
+            Drawable dr = new BitmapDrawable(getResources(), mutableBitmap);
+            dr.setBounds(
+                    0,
+                    0,
+                    mutableBitmap.getWidth(),
+                    mutableBitmap.getHeight()
+            );
+
+            ImageSpan isp = new ImageSpan(dr);
+            ssb.setSpan(
+                    isp,
+                    sp.length()-2,
+                    sp.length()-1,
+                    Spannable.SPAN_INCLUSIVE_INCLUSIVE
+            );
+            textView.append(ssb);
+
+            Snackbar.make(fab, "picture added", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+        }
+    }
+}
